72e372c6ca70d7df88b0a929c5a4197c0788c82d
zzjjaayy <jayprimerathod@gmail.com>
2020-08-23 14:02:09 +0530

    ScoreViewModel and ScoreViewModelFactory: Created a ViewModel and ViewModelFactory. The viewModelFactory is called passing in the score to make a new instance of the ScoreViewModel.

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -31,6 +32,9 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  * Fragment where the final score is shown, after the game is over
  */
 class ScoreFragment : Fragment() {
+
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -48,6 +52,12 @@ class ScoreFragment : Fragment() {
 
         // Get args using by navArgs property delegate
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
+
+        viewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
+        // the above line is to pass in the score to the viewModelFactory
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ScoreViewModel::class.java)
+        // the above line is to get that new instance of ScoreViewModel from the factory
+        // this will then provide a new ScoreViewModel
         binding.scoreText.text = scoreFragmentArgs.score.toString()
         binding.playAgainButton.setOnClickListener { onPlayAgain() }
 
